c9eafa1d7d74a0956dabd6e2d2d2cfb5dd31caaa
jsryudev <jsryu.dev@gmail.com>
2020-03-20 13:18:28 +0900

    프로젝트 초기 구성

---
+++ News Reader/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  News Reader
+//
+//  Created by 유준상 on 2020/03/20.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
