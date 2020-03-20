2bfafb5cc2bedcb245b990b4b6245a590f94671c
jsryudev <jsryu.dev@gmail.com>
2020-03-20 17:43:56 +0900

    News 모델 추가

---
+++ News Reader/Sources/Models/News.swift
@@ -0,0 +1,18 @@
+//
+//  News.swift
+//  News Reader
+//
+//  Created by 유준상 on 2020/03/20.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import Foundation
+
+typealias Keywords = (String, String, String)
+struct News {
+  let title: String
+  let url: URL
+  let content: String
+  let keywords: Keywords
+  let imageURL: URL
+}
