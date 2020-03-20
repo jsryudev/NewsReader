daa686e180c7ff2c18df814bd0eeabe34cbf1e83
jsryudev <jsryu.dev@gmail.com>
2020-03-21 01:04:23 +0900

    NewsCell, NewsCellReactor 추가

---
+++ News Reader/Sources/Views/NewsCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCell.swift
+//  News Reader
+//
+//  Created by 유준상 on 2020/03/21.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import RxSwift
+import SnapKit
+import ReactorKit
+
+class NewsCell: BaseTableViewCell {
+}
