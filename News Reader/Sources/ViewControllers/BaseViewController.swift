021b8e8dbc4494cee22d228ee019f1236c0d83ae
jsryudev <jsryu.dev@gmail.com>
2020-03-20 14:05:02 +0900

    사용하지 않는 ViewController 제거 및 BaseViewController 추가

---
+++ News Reader/Sources/ViewControllers/BaseViewController.swift
@@ -0,0 +1,53 @@
+//
+//  BaseViewController.swift
+//  News Reader
+//
+//  Created by 유준상 on 2020/03/20.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+class BaseViewController: UIViewController {
+
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required convenience init?(coder aDecoder: NSCoder) {
+    self.init()
+  }
+
+  var disposeBag = DisposeBag()
+
+  private(set) var didSetupConstraints = false
+
+  override func viewDidLoad() {
+    self.view.setNeedsUpdateConstraints()
+    self.addSubViews()
+    
+    self.navigationController?.navigationBar.prefersLargeTitles = true
+    
+    if #available(iOS 13.0, *) {
+      self.view.backgroundColor = .systemBackground
+    }
+  }
+
+  override func updateViewConstraints() {
+    if !self.didSetupConstraints {
+      self.setupConstraints()
+      self.didSetupConstraints = true
+    }
+    super.updateViewConstraints()
+  }
+
+  func addSubViews() {
+    // Override point
+  }
+
+  func setupConstraints() {
+    // Override point
+  }
+}
