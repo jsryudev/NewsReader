6638890615b2bff195329936ff02f02e7dfdc4ba
jsryudev <jsryu.dev@gmail.com>
2020-03-21 01:00:00 +0900

    BaseTableViewCell 추가

---
+++ News Reader/Sources/Views/BaseTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  BaseTableViewCell.swift
+//  News Reader
+//
+//  Created by 유준상 on 2020/03/21.
+//  Copyright © 2020 JunSang Ryu. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+class BaseTableViewCell: UITableViewCell {
+  var disposeBag: DisposeBag = DisposeBag()
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.initialize()
+    self.setupConstraints()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func initialize() {
+    // Override point
+  }
+
+  func setupConstraints() {
+    // Override point
+  }
+}
+
