706a362ff8b1a4a05993407154c3618140b13ca3
jsryudev <jsryu.dev@gmail.com>
2020-03-23 03:52:24 +0900

    키워드의 수가 모자른 경우의 예외처리 추가

--- News Reader/Sources/Models/News.swift
+++ News Reader/Sources/Models/News.swift
@@ -20,25 +20,33 @@ struct News {
 
 extension News {
   var keyewords: Keywords {
-    guard content.isNotEmpty else { return ("", "", "") }
     let tokens = self.content.split(separator: " ")
-    var dictionary = [String: Int]()
-    
-    tokens.forEach { token in
-      let key = String(token)
-      if let value = dictionary[key] {
-        dictionary[key] = value + 1
-      } else {
-        dictionary[key] = 1
+    guard tokens.isNotEmpty else { return ("", "", "") }
+   
+    let tokenCount = tokens.count
+    if tokenCount >= 3 {
+      var dictionary = [String: Int]()
+      
+      tokens.forEach { token in
+        let key = String(token)
+        if let value = dictionary[key] {
+          dictionary[key] = value + 1
+        } else {
+          dictionary[key] = 1
+        }
       }
+      
+      let sorted = dictionary.sorted { $0.1 > $1.1 }
+      
+      return (
+        sorted[0].key,
+        sorted[1].key,
+        sorted[2].key
+      )
+    } else if tokenCount == 2 {
+      return (String(tokens[0]), String(tokens[1]), "")
+    } else {
+      return (String(tokens[0]), "", "")
     }
-    
-    let sorted = dictionary.sorted { $0.1 > $1.1 }
-    
-    return (
-      sorted[0].key,
-      sorted[1].key,
-      sorted[2].key
-    )
   }
 }
