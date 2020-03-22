9e51991912bfa85eb643cafc362697e3612e5f3c
jsryudev <jsryu.dev@gmail.com>
2020-03-23 03:52:56 +0900

    _UIConstraintBasedLayoutLogUnsatisfiable 설정

--- News Reader/Sources/AppDelegate.swift
+++ News Reader/Sources/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     
+    UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+    
     let window = UIWindow(frame: UIScreen.main.bounds)
     if #available(iOS 13.0, *) {
       window.backgroundColor = .systemBackground
